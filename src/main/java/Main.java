ad2ccdc905cc66dcf0751be163b4700ec1e18093
Andrey Spiridonov <andrejvazapkin@gmail.com>
2022-03-20 00:01:47 +0400

    about age

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AgeVerify verify = new AgeVerify();
+
+        verify.ageVerify();
+        verify.ageVerify();
+        verify.ageVerify();
+
+    }
+}
